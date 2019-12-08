95fc8daf81ff04e40ef24c3f94e9f4460084ee30
Chase Ries <hello@chaseries.com>
2019-12-08 00:48:24 -0500

    Add basic error handling

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 import bi18n from "./lib/bi18n";
+import createStore from "./lib/store";
+import e from "./util/e.js";
 
 
 const cfgDefault = {
+  store: null,
   storeName: "bi18n",
-  componentName: "bi18n"
+  componentName: "bi18n",
+  languages: []
 };
 
 export default {
-  install (Vue, store, _cfg) {
 
-    const cfg = typeof _cfg === "undefined"
-      ? cfgDefault
-      : { ...cfgDefault, ..._cfg };
+  install (Vue, _cfg) {
 
-    if (typeof store === "undefined") {
-      throw new Error("bi18n depends on Vuex. Please pass a store into the plugin.");
-    }
 
-    store.registerModule(storeName);
+    if (typeof _cfg === "undefined") { throw new Error(e.cfgUndefined); }
+
+    if (typeof _cfg.store === "undefined") { throw new Error(e.cfgStoreUndefined); }
+
+    if (typeof _cfg.languages === "undefined") { throw new Error(e.cfgLanguagesUndefined); }
+
+    const cfg = { ...cfgDefault, ..._cfg };
+
+    cfg.store.registerModule(cfg.storeName, createStore(cfg.languages));
 
     Vue.component(cfg.componentName, bi18n(Vue));
   }
---
+++ util/e.js
@@ -0,0 +1,15 @@
+
+
+
+const cfgUndefined = "bi18n requires configuration. Please pass a configuration object as the second argument to `Vue.use` (e.g., `Vue.use(bi18n, cfg)`).";
+
+const cfgStoreUndefined = "Your configuration object must include the Vuex store you intend to use (format `{ store: myStore }`).";
+
+const cfgLanguagesUndefined = "Your configuration object must include the array of languages you intend to support. Recommended format uses string shortcodes (e.g., `{ languages: `['en', 'fr']`) }`";
+
+
+export default {
+  cfgUndefined,
+  cfgStoreUndefined,
+  cfgLanguagesUndefined
+};
